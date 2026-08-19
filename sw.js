0af500c4ade6dcd01c00e7b1f4238dee682dfa76
jmateuc <jaime.mateuc@gmail.com>
2026-08-19 20:43:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-const C="obra-companion-v1";const A=["./","index.html","style.css","app.js","manifest.webmanifest","assets/icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="obra-companion-v1.1";
+const A=["./","index.html","style.css","app.js","manifest.webmanifest","assets/icon.svg"];
+
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(a=>Promise.all(a.filter(x=>x!==C).map(x=>caches.delete(x))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(r=>{
+        const copy=r.clone();
+        caches.open(C).then(c=>c.put(e.request,copy));
+        return r;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
